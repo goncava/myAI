962ab16d54fdb50406e39bd49e2d44d385bb28f7
Gonzalo Cavallieri <Gonzalo_Cavallieri@kenan-flagler.unc.edu>
2025-02-21 17:23:27 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `[FARMER AI]`;
+export const OWNER_DESCRIPTION: string = `[An AI trained to guide aspiring farmers in the U.S., providing insights on regulations, financing, crop selection, and market trends]`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
