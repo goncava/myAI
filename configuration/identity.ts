0463640ba7ccf47cc8a161acc9455d15b61d9a70
Gonzalo Cavallieri <Gonzalo_Cavallieri@kenan-flagler.unc.edu>
2025-02-21 17:33:56 -0500

    FARMER AI

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `FARMER AI`;
 export const OWNER_DESCRIPTION: string = `[An AI trained to guide aspiring farmers in the U.S., providing insights on regulations, financing, crop selection, and market trends]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `FARMER AI`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
